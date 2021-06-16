67326db3c01fea876ab69c1d42df2ed8405548d8
Hillarie-kip <kalyahillary@gmail.com>
2021-06-16 19:55:48 +0300

    completed posting multiple images

--- Facebook/Controller/FaceBookController.cs
+++ Facebook/Controller/FaceBookController.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -60,6 +62,103 @@ namespace Facebook
             }
 
         }
-       
+
+
+
+
+
+        public static string PublishFBPost(string message, int AdID)
+        {
+
+
+
+            var postOnWallTask2 = PublishPost(message, AdID);
+            Task.WaitAll(postOnWallTask2);
+            //  var tokenResponse = JsonConvert.DeserializeObject<PublishImagesResponseModel>(postOnWallTask2.Result);
+            return postOnWallTask2.ToString();
+        }
+        public static async Task<string> PublishPost(string message, int AdID)
+        {
+
+
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(FacebookSettings.FB_BASE_ADDRESS);
+
+
+                List<KeyValuePair<string, string>> bodyProperties = new List<KeyValuePair<string, string>>();
+                //Loop over String array and add all instances to our bodyPoperties
+
+
+                var payments = GetImageList(AdID);
+                for (int i = 0; i < payments.Count; i++)
+                {
+                    var pay = payments[i];
+
+                    bodyProperties.Add(new KeyValuePair<string, string>("attached_media['" + pay.RowID + "']", "{media_fbid:'" + pay.FBImageID + "'}"));
+
+                }
+
+                //Create List of KeyValuePairs
+
+                //Add 'single' parameters
+                bodyProperties.Add(new KeyValuePair<string, string>("access_token", FacebookSettings.FB_ACCESS_TOKEN));
+                bodyProperties.Add(new KeyValuePair<string, string>("published", "true"));
+                bodyProperties.Add(new KeyValuePair<string, string>("message", message));
+
+
+                //convert your bodyProperties to an object of FormUrlEncodedContent
+                var dataContent = new FormUrlEncodedContent(bodyProperties.ToArray());
+
+                var req = new HttpRequestMessage(HttpMethod.Post, $"{FacebookSettings.FB_PAGE_ID}/feed") { Content = dataContent };
+                var res = await httpClient.SendAsync(req);
+
+
+                return await res.Content.ReadAsStringAsync();
+            }
+
+        }
+
+        //get list of saved images id in your db
+        public static List<ImagesModel> GetImageList(int AdID)
+        {
+            var payments = new List<ImagesModel>();
+            using (SqlConnection conn = new SqlConnection(""))
+            {
+                using (SqlCommand cmd = new SqlCommand($@"SELECT  ROW_NUMBER() OVER( ORDER BY ID )-1 AS 'RowID',* FROM MyHomeAdFacebookImage WHERE AdID=@AdID AND Status=2", conn))
+                {
+                    conn.Open();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@AdID", AdID);
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        var payment = new ImagesModel();
+                        payment.RowID = Convert.ToInt32(rdr["RowID"]);
+                        payment.ID = Convert.ToInt32(rdr["ID"]);
+                        payment.FBImageID = rdr["FBImageID"].ToString();
+
+                        payments.Add(payment);
+                    }
+                    conn.Close();
+                }
+            }
+
+
+            return payments;
+        }
+
+
+        public class ImagesModel
+        {
+            public int ID { get; set; }
+            public int RowID { get; set; }
+            public string ImageID { get; set; }
+            public string FBImageID { get; set; }
+            public string MainMessage { get; set; }
+            public int AdID { get; set; }
+        }
+
     }
 }
